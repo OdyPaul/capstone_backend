423277c57f3509360b3f9b362388f0f7a9ea5ee6
OdyPaul <aptsoriano0864@iskwela.psau.edu.ph>
2025-09-30 03:42:16 +0800

    protect

--- backend/routes/mobile/vcRoutes.js
+++ backend/routes/mobile/vcRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const vcCtrl = require('../../controllers/mobile/vcController');
+const { protect, admin } = require("../../middleware/authMiddleware");
 
 router.post('/', vcCtrl.createVCRequest);
 router.post('/:id/verify', vcCtrl.verifyRequest); // admin verifies
 // add GET list, GET single, etc. as needed
 
-router.get('/', vcCtrl.getVCRequests);       // list all
-router.get('/:id', vcCtrl.getVCRequestById); // single request
+router.get('/', protect, vcCtrl.getVCRequests);       // list all
+router.get('/:id',protect, vcCtrl.getVCRequestById); // single request
 
 module.exports = router;
