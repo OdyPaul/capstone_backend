efd460f394fef0c5d721b6a19f42d90d6709e86f
ody <aptsoriano0864@iskwela.psau.edu.ph>
2025-10-25 05:49:28 +0800

    gmail

--- backend/routes/mobile/googleVerifyRoutes.js
+++ backend/routes/mobile/googleVerifyRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const googleCtrl =require('../../controllers/mobile/googleVerify') 
+const googleCtrl = require('../../controllers/mobile/googleVerify');
 
+// POST /api/mobile/verify-gmail
+router.post('/verify-gmail', googleCtrl.verifyGoogleAccount);
 
-
-
-router.post("/verify-gmail",googleCtrl.verifyGoogleAccount )
+// 👇 You were missing this line
+module.exports = router;
