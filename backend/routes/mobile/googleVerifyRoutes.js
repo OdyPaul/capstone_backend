81b178a23a30d996150552fd93e2fa9849f5ca53
ody <aptsoriano0864@iskwela.psau.edu.ph>
2025-10-25 05:41:02 +0800

    Verify gmail

--- backend/routes/mobile/googleVerifyRoutes.js
+++ backend/routes/mobile/googleVerifyRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const googleCtrl =require('../../controllers/mobile/googleVerify') 
-const rateLimit = require('../../middleware/rateLimit'); 
 
 
 
-router.post("/verify-gmail", rateLimit(),googleCtrl.verifyGoogleAccount )
+
+router.post("/verify-gmail",googleCtrl.verifyGoogleAccount )
